c26f790997c128ce9c75ba8870074c828a43bfd0
i-atanasov <ivayloatanasov@zohomail.eu>
2025-07-14 00:04:30 +0300

    lint

--- src/Components/Home/Home.tsx
+++ src/Components/Home/Home.tsx
@@ -1,6 +1,5 @@
 import React from "react"
 import { useNavigate } from "react-router-dom";
-
 import { HomeContainer } from "./styles";
 import ProductField from "./ProductField";
 import CourseField from "../CourseField/CourseField";
