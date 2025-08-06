ebf680a501ff9c6b86c1a994cccb1740baec7f25
i-atanasov <ivayloatanasov@zohomail.eu>
2025-08-06 18:00:22 +0300

    server

---
+++ api/get.ts
@@ -0,0 +1,17 @@
+// Next.js example
+import postgres from 'postgres';
+
+let { PGHOST, PGDATABASE, PGUSER, PGPASSWORD } = process.env;
+
+const conn = postgres({
+  host: PGHOST,
+  database: PGDATABASE,
+  username: PGUSER,
+  password: PGPASSWORD,
+  port: 5432,
+  ssl: 'require',
+});
+
+export function selectAll() {
+  return conn('SELECT * FROM hello_world');
+}
