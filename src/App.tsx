482261969e0ff43088ae14eeb9d430ba3831d5c0
i-atanasov <ivayloatanasov@zohomail.eu>
2025-08-12 18:00:25 +0300

    analytics

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import Layout from './Components/Layout/Layout';
+import { Analytics } from "@vercel/analytics/next"
 import Router from './Components/Router/Router';
 import './App.css';
 
 export default function App() {
   return (
     <Layout>
-      <Router />
+        <Router />
+        <Analytics />
     </Layout>
   );
 }
