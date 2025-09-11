e34f09eb439a8bb4d515d38f17f4e1cac2b8667d
i-atanasov <ivayloatanasov@zohomail.eu>
2025-09-11 17:22:21 +0300

    mobile table

--- src/Components/Participants/styles.ts
+++ src/Components/Participants/styles.ts
@@ -4,6 +4,14 @@ export const ParticipantsWrapper = styled.div`
     min-height: 100vh;
     min-width: 80vw;
     padding: 20px;
+    font-family: 'Oswald', sans-serif;
+    font-size: 10px;
+    @media (min-width: 500px) {
+        font-size: 14px;
+    }
+    @media (min-width: 768px) {
+        font-size: 18px;
+    }
     > table {
         width: 100%;
         th {
